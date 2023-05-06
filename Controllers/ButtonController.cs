3d16b941a368f974010cfd74fcacbe029c082c8f
danstoyanov <dan.stoyanov@abv.bg>
2023-05-06 14:12:33 +0300

    Game Logic: ✅ ButtonController and Game Logic ✅ Button View Position

--- Controllers/ButtonController.cs
+++ Controllers/ButtonController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FantasyButtonWebGame.Models;
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace FantasyButtonWebGame.Controllers
 {
     public class ButtonController : Controller
     {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+
         public IActionResult Index()
         {
-            return View();
+            if (buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel { Id = i, ButtonState = random.Next(4) });
+                }
+            }
+
+            return View("Index", buttons);
         }
     }
 }
